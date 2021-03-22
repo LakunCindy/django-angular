771a884d6d605b5d0d466cb820b050beec844139
Haru <Haruku91@outlook.fr>
2021-03-22 16:04:39 +0100

    commut

--- TME_webAPI_DJVAWS/mySearchEngine/myCost/models.py
+++ TME_webAPI_DJVAWS/mySearchEngine/myCost/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Cost(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.CharField(default='',max_length=10)
     tigId = models.IntegerField(default='-1')
     quantity = models.IntegerField(default='0')
     totalPrice = models.IntegerField(default='0')
 
     class Meta:
-        ordering = ('tigId','quantity','totalPrice',)
+        ordering = ('created','tigId','quantity','totalPrice',)
