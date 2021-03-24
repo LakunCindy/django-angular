f81d1199345ccd26ae0c9cf390abf5cdc9e51272
Eric <Eric@DESKTOP-MUDF02N>
2021-03-24 10:56:22 +0100

    ajout du sous-component year gain

---
+++ BTBO2/src/app/component/sub-component/year-gain/year-gain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-year-gain',
+  templateUrl: './year-gain.component.html',
+  styleUrls: ['./year-gain.component.css']
+})
+export class YearGainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
