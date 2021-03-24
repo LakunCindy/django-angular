5fec03ef031d88fed5fe67b7da49562fd051ce2b
Eric <Eric@DESKTOP-MUDF02N>
2021-03-24 10:55:53 +0100

    ajout du sous-component month gain

---
+++ BTBO2/src/app/component/sub-component/month-gain/month-gain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-month-gain',
+  templateUrl: './month-gain.component.html',
+  styleUrls: ['./month-gain.component.css']
+})
+export class MonthGainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
